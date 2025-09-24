c17e3f115fbacd89bc9e061f4bd271c51203922d
ruslan_uterly <kholikov.rus@mail.ru>
2025-09-24 15:56:25 -0400

    add frontend ecosystem

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: __dirname + '/.env.deploy' });
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      key: "~/.ssh/vm_access/ssh-key-1758536918231",
+      "post-deploy": 'cd frontend && pwd && export NODE_OPTIONS=--openssl-legacy-provider && npm i && npm run build'
+    },
+  },
+}; 
